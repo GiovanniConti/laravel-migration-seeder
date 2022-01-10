a46ae5943e1be63af93b565406374a4428b90b8b
GiovanniConti <giovanni.conti@live.it>
2022-01-10 19:56:57 +0100

    created travel_packages migration

---
+++ database/migrations/2022_01_10_180729_create_travel_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel_packages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 100);
+            $table->string('description');
+            $table->smallInteger('date');
+            $table->tinyInteger('nights');
+            $table->decimal('price', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel_packages');
+    }
+}
